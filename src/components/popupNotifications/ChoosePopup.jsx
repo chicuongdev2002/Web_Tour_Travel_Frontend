02f47dc0cfaad0204916cd84550acf484b8f7a70
Sơn Nguyễn <sonnt1@imt-soft.com>
2024-11-09 12:57:48 +0700

    fix render pop notify

--- src/components/popupNotifications/ChoosePopup.jsx
+++ src/components/popupNotifications/ChoosePopup.jsx
@@ -6,7 +6,7 @@ import DrawQuestionMark from '../draw/DrawQuestionMark'
 function ChoosePopup({ title, message, open, onclose, onAccept, onReject }) {
     return (
         <ModalComponent open={open} onclose={onclose}>
-            <div className='divCenterColumnBetween' style={{ width: title.length > 15? 400 : 200, 
+            <div className='divCenterColumnBetween' style={{ width: title.length > 15 || message.length > 20? 400 : 200, 
                 height: 210, marginTop: 10}}>
                 <div className='divCenter' style={{ width: 200, height: 100 }}>
                     <DrawQuestionMark duration={2}/>
