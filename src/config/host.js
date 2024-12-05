37d623a05cc552dd900e6838f2e9580c580d9f69
Sơn Nguyễn <sonnt1@imt-soft.com>
2024-12-06 01:19:33 +0700

    integration chat into project

--- src/config/host.js
+++ src/config/host.js
@@ -41,6 +41,8 @@ const GET_PAYMENT_BY_BOOKING = REACT_APP_HOST + '/api/payment/booking'
 const GET_TOUR_TYPE = REACT_APP_HOST + '/api/tours/tourType'
 const GET_BOOKING = REACT_APP_HOST + '/api/bookings/getBookingById'
 const CANCEL_BOOKING = REACT_APP_HOST + '/api/bookings/cancelBooking'
+const API_CHAT = 'https://api.dify.ai/v1/chat-messages';
+const TOKEN_CHAT = 'Bearer app-wkO00LhNJwAYYs3dtIFaBoL9';
 const getAPI = (api, params, paramId) => {
   if (paramId) {
     api += "/" + paramId;
@@ -64,4 +66,5 @@ export { GET_ALL_TOUR, CHECK_ACCOUNT_EXISTS, CHECK_USER_EXISTS, BOOKING_TOUR, GE
     STATIS_TOUR_REVIEW,
     REVIEW_MONTHLY_STATIS,
     STATIS_REVENUE_TOTAL,
-    GET_ALL_TOUR_TEST, GET_TOUR_TYPE, GET_BOOKING, CANCEL_BOOKING, getAPI };
+    GET_ALL_TOUR_TEST, GET_TOUR_TYPE, GET_BOOKING, CANCEL_BOOKING, API_CHAT,
+    TOKEN_CHAT, getAPI };
